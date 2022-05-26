e04cf186e73be88eb28ae8f829f797b928484e78
SBIK1219 <sbikjbi@gmail.com>
2022-05-27 00:37:51 +0900

    Evaluating commit

--- 05_Classification.R
+++ 05_Classification.R
@@ -146,3 +146,48 @@ graph <- ggplot(df, aes(x=payment_inc_ratio, y=partial_resid, solid = FALSE)) +
   coord_cartesian(xlim=c(0, 25)) +
   theme_bw()
 graph
+
+pred <- predict(logistic_gam, newdata=loan_data)
+pred_y <- as.numeric(pred > 0)
+true_y <- as.numeric(loan_data$outcome=='default')
+true_pos <- (true_y==1) & (pred_y==1)
+true_neg <- (true_y==0) & (pred_y==0)
+false_pos <- (true_y==0) & (pred_y==1)
+false_neg <- (true_y==1) & (pred_y==0)
+conf_mat <- matrix(c(sum(true_pos), sum(false_pos),
+                     sum(false_neg), sum(true_neg)), 2, 2)
+colnames(conf_mat) <- c('Yhat = 1', 'Yhat = 0')
+rownames(conf_mat) <- c('Y = 1', 'Y = 0')
+conf_mat
+
+# precision
+conf_mat[1, 1] / sum(conf_mat[,1])
+# recall
+conf_mat[1, 1] / sum(conf_mat[1,])
+# specificity
+conf_mat[2, 2] / sum(conf_mat[2,])
+
+idx <- order(-pred)
+recall <- cumsum(true_y[idx] == 1) / sum(true_y == 1)
+specificity <- (sum(true_y == 0) - cumsum(true_y[idx] == 0)) / sum(true_y == 0)
+roc_df <- data.frame(recall = recall, specificity = specificity)
+
+graph <- ggplot(roc_df, aes(x=specificity, y=recall)) +
+  geom_line(color='blue') + 
+  scale_x_reverse(expand=c(0, 0)) +
+  scale_y_continuous(expand=c(0, 0)) + 
+  geom_line(data=data.frame(x=(0:100) / 100), aes(x=x, y=1-x),
+            linetype='dotted', color='red') +
+  theme_bw() + theme(plot.margin=unit(c(5.5, 10, 5.5, 5.5), "points"))
+graph
+
+sum(roc_df$recall[-1] * diff(1-roc_df$specificity))
+head(roc_df)
+
+graph <- ggplot(roc_df, aes(specificity)) +
+  geom_ribbon(aes(ymin=0, ymax=recall), fill='blue', alpha=.3) +
+  scale_x_reverse(expand=c(0, 0)) +
+  scale_y_continuous(expand=c(0, 0)) +
+  labs(y='recall') +
+  theme_bw() + theme(plot.margin=unit(c(5.5, 10, 5.5, 5.5), "points"))
+graph
