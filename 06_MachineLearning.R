cb8c92b2184a9f4efba62f2d7875fe1ef76d2c5a
SBIK1219 <sbikjbi@gmail.com>
2022-06-01 09:02:19 +0900

    Machine Learning commit

--- 06_MachineLearning.R
+++ 06_MachineLearning.R
@@ -228,3 +228,107 @@ graph <- ggplot(imp) +
     panel.grid.major.y = element_line(linetype=3, color="darkgray") ) +
   theme_bw()
 graph
+
+predictors <- data.matrix(loan3000[, c('borrower_score', 'payment_inc_ratio')])
+label <- as.numeric(loan3000[,'outcome']) - 1
+xgb <- xgboost(data=predictors, label=label, objective='binary:logistic', 
+               params=list(subsample=0.63, eta=0.1), nrounds=100, 
+               eval_metric='error')
+
+
+pred <- predict(xgb, newdata=predictors)
+xgb_df <- cbind(loan3000, pred_default = pred > 0.5, prob_default = pred)
+
+graph <- ggplot(data=xgb_df, aes(x=borrower_score, y=payment_inc_ratio, 
+                                 color=pred_default, shape=pred_default)) +
+  geom_point(alpha=0.6, size=2) +
+  scale_shape_manual( values=c(46, 4)) +
+  scale_x_continuous(expand=c(0.03, 0)) + 
+  scale_y_continuous(expand=c(0, 0)) + 
+  coord_cartesian(ylim=c(0, 20)) +
+  theme_bw()
+graph
+
+graph <- ggplot(data=xgb_df, aes(x=borrower_score, y=payment_inc_ratio, 
+                                 color=pred_default, shape=pred_default, size=pred_default)) +
+  geom_point(alpha=.8) +
+  scale_color_manual(values = c('FALSE'='#b8e186', 'TRUE'='#d95f02')) +
+  scale_shape_manual(values = c('FALSE'=0, 'TRUE'=1)) +
+  scale_size_manual(values = c('FALSE'=0.5, 'TRUE'=2)) +
+  
+  scale_x_continuous(expand=c(0.03, 0)) + 
+  scale_y_continuous(expand=c(0, 0)) + 
+  coord_cartesian(ylim=c(0, 20)) +
+  guides(color = guide_legend(override.aes = list(size=2))) +
+  theme_bw()
+graph
+
+seed <- 400820
+predictors <- data.matrix(loan_data[,-which(names(loan_data) %in% 'outcome')])
+label <- as.numeric(loan_data$outcome)-1
+test_idx <- sample(nrow(loan_data), 10000)
+
+xgb_default <- xgboost(data=predictors[-test_idx,], label=label[-test_idx], 
+                       objective='binary:logistic', nrounds=250, verbose=0, 
+                       eval_metric='error')
+pred_default <- predict(xgb_default, predictors[test_idx,])
+error_default <- abs(label[test_idx] - pred_default) > 0.5
+xgb_default$evaluation_log[250,]
+mean(error_default)
+
+xgb_penalty <- xgboost(data=predictors[-test_idx,], label=label[-test_idx], 
+                       params=list(eta=.1, subsample=.63, lambda=1000),
+                       objective='binary:logistic', nrounds=250, verbose=0, 
+                       eval_metric='error')
+pred_penalty <- predict(xgb_penalty, predictors[test_idx,])
+error_penalty <- abs(label[test_idx] - pred_penalty) > 0.5
+xgb_penalty$evaluation_log[250,]
+mean(error_penalty)
+
+error_default <- rep(0, 250)
+error_penalty <- rep(0, 250)
+for(i in 1:250) {
+  pred_default <- predict(xgb_default, predictors[test_idx,], iterationrange=c(1, i))
+  error_default[i] <- mean(abs(label[test_idx] - pred_default) > 0.5)
+  pred_penalty <- predict(xgb_penalty, predictors[test_idx,], iterationrange=c(1, i))
+  error_penalty[i] <- mean(abs(label[test_idx] - pred_penalty) > 0.5)
+}
+
+errors <- rbind(xgb_default$evaluation_log,
+                xgb_penalty$evaluation_log,
+                data.frame(iter=1:250, train_error=error_default),
+                data.frame(iter=1:250, train_error=error_penalty))
+errors$type <- rep(c('default train', 'penalty train', 
+                     'default test', 'penalty test'), rep(250, 4))
+
+graph <- ggplot(errors, aes(x=iter, y=train_error, group=type)) +
+  geom_line(aes(linetype=type, color=type), size=1) +
+  scale_linetype_manual(values=c('solid', 'dashed', 'dotted', 'longdash')) +
+  theme_bw() +
+  theme(legend.key.width = unit(1.5,"cm")) +
+  labs(x="Iterations", y="Error") +
+  guides(color = guide_legend(override.aes = list(size=1)))
+graph
+
+N <- nrow(loan_data)
+fold_number <- sample(1:5, N, replace=TRUE)
+params <- data.frame(eta = rep(c(.1, .5, .9), 3),
+                     max_depth = rep(c(3, 6, 12), rep(3,3)))
+rf_list <- vector('list', 9)
+error <- matrix(0, nrow=9, ncol=5)
+for(i in 1:nrow(params)){
+  for(k in 1:5){
+    cat('Fold', k, 'for model', i, '\n')
+    fold_idx <- (1:N)[fold_number == k]
+    xgb <- xgboost(data=predictors[-fold_idx,], label=label[-fold_idx], 
+                   params=list(eta=params[i, 'eta'], 
+                               max_depth=params[i, 'max_depth']),
+                   objective='binary:logistic', nrounds=100, verbose=0, 
+                   eval_metric='error')
+    pred <- predict(xgb, predictors[fold_idx,])
+    error[i, k] <- mean(abs(label[fold_idx] - pred) >= 0.5)
+  }
+}
+
+avg_error <- 100 * round(rowMeans(error), 4)
+cbind(params, avg_error)
